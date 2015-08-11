bbb771ff7b50b6b40b237ab041071d9f6647b386
Alberto Galera <galerajimenez@gmail.com>
2015-08-11 15:00:32 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='basicevents',
+    version='0.1',
+    description='python events non-blocking',
+    long_description='python basic and easy events non-blocking',
+    author='Alberto Galera Jimenez',
+    author_email='galerajimenez@gmail.com',
+    url='https://github.com/kianxineki/basicevents',
+    py_modules=['basicevents'],
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="GPL",
+    zip_safe=False,
+    keywords='basicevents',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    entry_points={}
+)
