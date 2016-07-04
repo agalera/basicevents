9446d4042271d5a247ddecaa7f633aec005b4e94
Alberto Galera <galerajimenez@gmail.com>
2016-07-04 10:31:36 +0200

    py2to3 examples

--- examples/example1.py
+++ examples/example1.py
@@ -4,23 +4,23 @@ from basicevents import (subscribe, send_thread, send_queue,
 
 @subscribe("Hello1")
 def print_message(*args, **kwargs):
-    print "Example function1", args, kwargs
+    print("Example function1", args, kwargs)
 
 
 @subscribe("Hello2")
 def print_message2(*args, **kwargs):
     import time
     time.sleep(4)
-    print "Example function2", args, kwargs
+    print("Example function2", args, kwargs)
 
 
 @subscribe("STOP")
 def dead_mainthread(*args, **kwargs):
-    print "dead MainThread!"
+    print("dead MainThread!")
 
 
 def other_example(*args, **kwargs):
-    print "manual subscribe"
+    print("manual subscribe")
 
 
 send_thread("Hello1", text_example="new thread")
